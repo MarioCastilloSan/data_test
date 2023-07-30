73049820706babb5457f45277f697651cd07a258
Mario <mariocastillosan@gmail.com>
2023-07-30 02:03:31 -0400

    requirements

--- app.py
+++ app.py
@@ -1,10 +1,7 @@
 import streamlit as st
 import pandas as pd
 from reg import newPrediction,readData
-import subprocess
-import sklearn
 data = readData()
-subprocess.run("pip install scikit-learn", shell=True)
 def main():
     st.title('Modelo de Predicción de Compras')
 
